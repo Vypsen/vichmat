b7ae306afe49274e5ae13f90a50439be0ce75693
Sergei Semenets <s.semenets2001@mail.ru>
2022-03-10 02:07:05 +1000

    add test and perform main elem

--- gaus/main_elem.py
+++ gaus/main_elem.py
@@ -3,28 +3,24 @@ import numpy as np
 def mainGaus(matrix, vector):
     index_main_elem = np.unravel_index(np.argmax(abs(matrix)), matrix.shape)
     main_elem = matrix[index_main_elem]
-    main_line = matrix[index_main_elem[0]]
+    main_line = matrix[index_main_elem[0]].copy()
     main_vector = vector[index_main_elem[0]]
     
-    if len(vector) == 1:
-        first_root = [0]*matrix.shape[1]
-        first_root[index_main_elem[1]] = vector[index_main_elem[0]]/matrix[index_main_elem]
+    if main_elem == 0:
+        return [0]*matrix.shape[1]
+    np.putmask(matrix[index_main_elem[0]], [True]*matrix.shape[0], 0)
+    
+    for i in range(matrix.shape[0]):
+        multiplier = matrix[(i,index_main_elem[1])]/main_elem
+        matrix[i] -= main_line*multiplier 
+        vector[i] -= main_vector*multiplier
         
-        return first_root
-    
-    new_matrix = np.delete(matrix, index_main_elem[0], 0)
-    new_vector = np.delete(vector, index_main_elem[0], 0)
-    
-    for i in range(new_matrix.shape[0]):
-        multiplier = abs(new_matrix[(i,index_main_elem[1])]/main_elem)
-        new_matrix[i] += main_line*multiplier 
-        new_vector[i] += main_vector*multiplier
+    answer = mainGaus(matrix, vector)  
      
-    answer = mainGaus(new_matrix, new_vector)
-    
-    answer[index_main_elem[1]] = (
-        vector[index_main_elem[0]]/matrix[index_main_elem]) - (
-        sum(list(alpha/main_elem * answer[matrix.tolist()[index_main_elem[0]].index(alpha)] 
-            for alpha in matrix.tolist()[index_main_elem[0]])))
-        
-    return(answer)
+    root = main_vector/main_elem
+    for alpha in main_line.tolist():
+        root -= alpha/main_elem * answer[main_line.tolist().index(alpha)]
+        main_line[main_line.tolist().index(alpha)] = 0        
+    answer[index_main_elem[1]] = root
+            
+    return answer
--- index.py
+++ index.py
@@ -2,17 +2,18 @@ from gaus.main_elem import mainGaus
 from scipy.linalg import solve, det
 import numpy as np
 import task
-from colorama import init, Fore, Style
+from colorama import init, Fore
 init()
 
 if __name__ == '__main__':
     
-    matrix = task.matrix
-    vector = task.vector
+    matrix = task.matrix.astype('float64')
+    vector = task.vector.astype('float64')
+
     
     if det(matrix) != 0:
-        answer = np.round(solve(matrix, vector), 5)
-        ansverGaus = np.round(mainGaus(matrix, vector),5)
+        answer = np.round(solve(matrix, vector), 7)
+        ansverGaus = np.round(mainGaus(matrix, vector),7)
         
         print(Fore.YELLOW, 'выбор главного элемента:', ansverGaus)
         print(Fore.GREEN, 'питоновский ответ:', answer)
