a167c8cf0eec78d76ae01d1ec82e268f9786aa6d
Sergei Semenets <s.semenets2001@mail.ru>
2022-03-30 01:09:34 +1000

    form check methods

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from gaus.main_elem import mainGaus
+from gaus.LU import LuDecomposition
 from gaus.optimal import rect_method
 from scipy.linalg import solve, det
 import numpy as np
@@ -6,25 +7,31 @@ import task
 from colorama import init, Fore
 init()
 
+
+
+def output_check(method_text, method):
+    answer = np.round(solve(matrix, vector), 7)  
+    color = Fore.GREEN if (answer == method).all() else Fore.RED
+    print(color, method_text, method)
+
 if __name__ == '__main__':
     
     matrix = task.matrix.astype('float64')
     vector = task.vector.astype('float64')
-
+    
     if det(matrix) != 0:
-        ansverOptimalExc = np.round(rect_method(matrix, vector), 7)
-        answer = np.round(solve(matrix, vector), 7)
-        ansverMainGaus = np.round(mainGaus(matrix, vector), 7)
         
-        print(Fore.YELLOW, 'выбор главного элемента:', ansverMainGaus)
-        print(Fore.YELLOW, 'метод оптимального исключения:', ansverOptimalExc)
-
-        print(Fore.GREEN, 'питоновский ответ:', answer)
-        print(Fore.RED, (answer == ansverMainGaus).all())
-        print(Fore.RED, (answer == ansverOptimalExc).all())
-
+        ansverMainGaus = np.round(mainGaus(matrix, vector), 7)
+        ansverOptimalExc = np.round(rect_method(matrix, vector), 7)
+        ansverLU = np.round(LuDecomposition(matrix, vector), 7)
+            
+        output_check('выбор главного элемента:', ansverMainGaus)
+        output_check('метод оптимального исключения:', ansverOptimalExc)
+        output_check('LU разложение:', ansverLU)
+        
     else:
         print(Fore.RED + 'матрица выроженная')
+        
         
 
         
