78ce3aacdacb907fbbbe910da6dc56e166e08e5a
Vypsen <s.semenets2001@mail.ru>
2022-04-30 21:21:09 +1000

    simple iteration and Seidel method

---
+++ methods/simpleIteration.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+def checkAccuracy(x, x0):
+    eps = 0.0000001
+    for i in range(x.shape[0]):
+        print(abs(x[i] - x0[i]))
+        if (abs(x[i] - x0[i]) > eps):
+            return False
+            
+    return True
+
+
+def calrResultSimpleIteration(matrix, vector):
+    
+    x = np.zeros(matrix.shape[0])
+    step = 1
+    while(True):
+        x1 = np.zeros(matrix.shape[0])
+        for i in range(matrix.shape[0]):
+            x1[i] = (vector[i] - sum(matrix[i][j]*x[j] for j in range(matrix.shape[0])) + matrix[i][i]*x[i])/matrix[i][i]
+        print(str(step) + " step: " + str(x1))
+        print(x)
+        step += 1
+        if(checkAccuracy(x1, x)):
+            x = x1.copy()
+            break
+        x = x1.copy()
+
+    return x
+
+
+def calrResultMethodSeidel(matrix, vector):
+    x = np.zeros(matrix.shape[0])
+    step = 1
+    while(True):
+        x1 = np.zeros(matrix.shape[0])
+        x0 = x.copy()
+        for i in range(matrix.shape[0]):
+            x1[i] = (vector[i] - sum(matrix[i][j]*x[j] for j in range(matrix.shape[0])) + matrix[i][i]*x[i])/matrix[i][i]
+            x[i] = x1[i]
+        print(str(step) + " step: " + str(x1))
+        step += 1
+        if(checkAccuracy(x1, x0)):
+            x = x1.copy()
+            break
+        x = x1.copy()
+
+    return x
+
+
+
+
+def simpleIteration(matrix, vector):
+
+    x = calrResultSimpleIteration(matrix, vector)
+    return x
+
+def methodSeidel(matrix, vector):
+
+    x = calrResultMethodSeidel(matrix, vector)
+    return x
