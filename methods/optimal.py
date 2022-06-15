b7e47db741a2372ca8974e8ad6eabc8cb673d802
Vypsen <s.semenets2001@mail.ru>
2022-06-15 21:17:07 +1000

    optimal

---
+++ methods/optimal.py
@@ -0,0 +1,20 @@
+def rect_method(matrix, vector):
+    matrix = matrix.copy()
+    vector = vector.copy()
+    for row in range(matrix.shape[0]):    
+        main_elem = matrix[row][row]
+          
+        matrix[row] /= main_elem
+        vector[row] /= main_elem
+                    
+        for index_row in range(row - 1, -1, -1): 
+            matrix[index_row] -= matrix[row]*matrix[index_row][row]
+            vector[index_row] -= vector[row]*matrix[index_row][row]
+            
+        if row + 1 == matrix.shape[0]:
+            return vector
+        
+        for index_row in range(row, -1, -1): 
+            matrix[row + 1] -= matrix[index_row]*matrix[row + 1][index_row]        
+            vector[row + 1] -= vector[index_row]*matrix[row + 1][index_row] 
+    
