05ed2670aada406adce2676c6fc101ef7b6f9cc1
Vypsen <s.semenets2001@mail.ru>
2022-06-15 21:15:34 +1000

    main fail, matrixs and help func

--- index.py
+++ index.py
@@ -1,34 +1,33 @@
-from gaus.main_elem import mainGaus
-from gaus.LU import LuDecomposition
-from gaus.optimal import rect_method
-from scipy.linalg import solve, det
+from methods.simpleSelfValue import findMaxSelf
+from methods.straightIteration import reverseIteration
+from methods.richardson import richardson
+from methods.allSelfMean import selfMeanMethodSim
+
 import numpy as np
 import task
+from scipy.linalg import det, eig
 from colorama import init, Fore
+
 init()
-
-
-
-def output_check(method_text, method):
-    answer = np.round(solve(matrix, vector), 7)  
-    color = Fore.GREEN if (answer == method).all() else Fore.RED
-    print(color, method_text, method)
 
 if __name__ == '__main__':
     
-    matrix = task.matrix.astype('float64')
-    vector = task.vector.astype('float64')
+    matrix = task.matrix
+    vector = task.vector
+
+    N = 10
+    matrix = np.random.random_integers(-100,100,size=(N,N))
+    matrix_symm = (matrix + matrix.T)/2
+    print('Матрица:')
+    print(matrix_symm)
+    w, v = eig(matrix_symm)
     
-    if det(matrix) != 0:
-        
-        ansverMainGaus = np.round(mainGaus(matrix, vector), 7)
-        ansverOptimalExc = np.round(rect_method(matrix, vector), 7)
-        ansverLU = np.round(LuDecomposition(matrix, vector), 7)
-            
-        output_check('выбор главного элемента:', ansverMainGaus)
-        output_check('метод оптимального исключения:', ansverOptimalExc)
-        output_check('LU разложение:', ansverLU)
-        
+    if det(matrix_symm) != 0:
+        answer = np.array(sorted(selfMeanMethodSim(matrix_symm, 10)))
+        w = np.array(sorted(w))
+        print('собственные через scipy.linalg:', w)
+        print('собственные значения методом вращение Якоби:',answer)
+        print('разница между решением scipy.linalg и методом вращение Якоби',w - answer)
     else:
         print(Fore.RED + 'матрица выроженная')
         
